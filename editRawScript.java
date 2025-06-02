7b0f360f30b399b6ea5087be6f8c95ad3d842f00
Meghana Madiraju <meghanambpd@gmail.com>
2025-06-02 00:50:39 -0400

    made more efficient and renamed

---
+++ editRawScript.java
@@ -0,0 +1,49 @@
+//remove all scene description lines, CUT TOs:, (beat)s, and those weird page number lines
+
+import java.io.*;
+import java.util.Scanner;
+
+public class editRawScript {
+    public static void main(String[] args) throws IOException {
+        String scriptPath = "C:\\Users\\megha\\zucktest\\socialnetworkRaw.txt";
+        String newpath = "C:\\Users\\megha\\zucktest\\socialnetwork.txt";
+        File file = new File(scriptPath);
+        try (Scanner davidfinch = new Scanner(file);
+                FileWriter sorkin = new FileWriter(newpath, true)) {
+            while (davidfinch.hasNextLine()) {
+                String line = davidfinch.nextLine().trim();
+                if (!line.matches(".*(INT\\.|EXT\\.|CUT TO:|CUT BACK TO:).*") &&
+                        !line.matches("^\\(.*\\)$") && !line.matches("^\\d{1,3} \\.$")) {
+                    sorkin.write(line + "\n");
+                }
+
+                // if the line has 10 spaces (11 words) and is more than 2 sentences then split
+                // the line up
+                if ((countChar(line, ' ') > 10) && countChar(line, '.') > 2) {
+                    for (int i = 0; i < line.length(); i++) {
+                        if (line.charAt(i) == '.') {
+                            String sentence = null;
+                            for (int j = 0; j < i + 1; j++) {
+                                sentence += line.charAt(j);
+                                sorkin.write(sentence + "\n");
+                            }
+                            line = line.substring(i, line.length()); // holy shoot I feel like a genius
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static int countChar(String line, char cha) {
+        int count = 0;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == cha) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
