cde42a3d94a52d05252ec345814d2bb6949a2e08
Meghana Madiraju <meghanambpd@gmail.com>
2025-06-05 23:18:03 -0400

    fixed ?s and added points system
    then another ? issue came up with em dashes (awesome...)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 //things to add:
-//fix ?s with splitting
+//fix stupid scanner problem
+//fix ?s with em dashes
 //split up long monologues (>2 sentences) (looking at you justin timberlake)
-//if the firstHalf has <3 words then add a word from the second half to the first
 //you get points for how many letters you match with SecondHalf
 //hard mode includes punctuation
 //remove lines from irrelevant characters (?)
@@ -9,23 +9,43 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static File dialogueFile = new File("C:\\Users\\megha\\zucktest\\dialogue.txt");
     public static String linesPath = "C:\\Users\\megha\\zucktest\\lines.txt";
 
     public static void main(String[] args) throws IOException {
-        File file = new File(linesPath);
-        Scanner finch = new Scanner(file);
-        Scanner dook = new Scanner(System.in);
         boolean play = true;
-        removeShort();
+        int points = 0;
 
         while (play) {
+            Scanner dook = new Scanner(System.in);
             String randLine = randomLine();
             String firstHalf = getFirstHalf(randLine);
-            String cans = randLine.substring(firstHalf.length()).trim(); // dude I am so smart (cans = correct answer)
+            String cans = getSecondHalf(randLine).trim(); // cans = correct answer
+
+            // if the firstHalf has <4 words then add a word from the second half to the
+            // first
+            // I was going to put this in getFirstHalf() but you need the second half to do
+            // this properly
+            // and it's all very sad
+            if (countSpaces(firstHalf) < 3) {
+                String extraWord = "";
+                for (int i = 0; i < cans.length(); i++) {
+                    while (cans.charAt(i) != ' ') {
+                        extraWord += cans.charAt(i);
+                    }
+                }
+                cans = cans.substring(extraWord.length()); // okay this feels cool
+                extraWord += " " + firstHalf;
+                firstHalf = extraWord; // god this is depressing
+
+            }
+
             // make it all lowercase and remove all punctuation
             String cansFiltered = cans.toLowerCase().replaceAll("[\\.!,\\?:;\\-']", "");
 
@@ -36,12 +56,16 @@ public class Main {
 
             if (pansFiltered.equalsIgnoreCase(cansFiltered)) {
                 System.out.println("Congrats, you need to get outside more.");
-                System.out.println("");
+                points += matcher(pans, cans);
+                System.out.println("Your score is " + points);
             } else if (pans.equalsIgnoreCase("quit")) {
                 dook.close();
+                System.out.print("Your score was " + points);
                 play = false;
             } else {
-                System.out.println("Answer: " + cans + "\n");
+                System.out.println("Answer: " + cans);
+                points += matcher(pans, cans);
+                System.out.println("Your score is " + points);
             }
         }
 
@@ -53,7 +77,7 @@ public class Main {
             int rand = (int) Math.floor(Math.random() * (numLines - 1));
             int currentLineNum = 1;
             String line;
-            
+
             // is this the most efficient way to do this? I have no idea. Also I need to
             // decide between BufferedReader and FileReader and Scanner
             while ((line = reader.readLine().trim()) != null) {
@@ -83,28 +107,51 @@ public class Main {
 
         StringBuilder firstHalfBuilder = new StringBuilder();
 
-        for (int i = 0; i < words.length; i++) {
-            if (i < mid) {
-                firstHalfBuilder.append(words[i]).append(" ");
-            }
+        for (int i = 0; i < mid; i++) {
+            firstHalfBuilder.append(words[i]).append(" ");
         }
 
         String firstHalf = firstHalfBuilder.toString().trim();
+
         return firstHalf;
     }
 
-    public static void removeShort() throws IOException {
-        FileWriter cloo;
-        try (Scanner bloo = new Scanner(dialogueFile)) {
-            cloo = new FileWriter(linesPath, true);
-            while (bloo.hasNextLine()) {
-                String line = bloo.nextLine().trim();
+    public static String getSecondHalf(String line) throws IOException {
+        String[] words = line.split(" ");
+        int mid = words.length / 2;
 
-                if ((countSpaces(line) > 3)) {
-                    cloo.write(line + "\n");
-                }
+        StringBuilder secondHalfBuilder = new StringBuilder();
+
+        for (int i = mid; i < words.length; i++) {
+            secondHalfBuilder.append(words[i]).append(" ");
+
+        }
+
+        String secondHalf = secondHalfBuilder.toString().trim();
+        return secondHalf;
+    }
+
+    public static int matcher(String pans, String cans) {
+        // Normalize: lowercase + remove punctuation
+        pans = pans.toLowerCase().replaceAll("[^a-z0-9 ]", "");
+        cans = cans.toLowerCase().replaceAll("[^a-z0-9 ]", "");
+
+        // Split into words
+        String[] pansWords = pans.split("\\s+");
+        String[] cansWords = cans.split("\\s+");
+
+        // Add words from string1 to a set
+        Set<String> wordSet = new HashSet<>(Arrays.asList(pansWords));
+
+        int count = 0;
+        for (String word : cansWords) {
+            if (wordSet.contains(word)) {
+                count++;
+                wordSet.remove(word); // Optional: prevent double-counting
             }
         }
-        cloo.close();
+
+        return count;
+
     }
 }
